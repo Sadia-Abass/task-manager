0d6e5d79c35ab53e170790599574201deccdc2f5
Sadia Abass <sadia.abass@outlook.com>
2025-09-19 23:24:06 +0100

    Add unit of work interface.

---
+++ EasyManagerApp/server/EasyManagerApp.API/EasyManagerApp.API/Domain/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,8 @@
+﻿namespace EasyManagerApp.API.Domain.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IGroupRepository GroupRepository { get; }
+        Task<int> SaveChangesAsync();
+    }
+}
