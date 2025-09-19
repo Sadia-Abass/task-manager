63c8560f810793e14648e24659632f7996ab4e0e
Sadia Abass <sadia.abass@outlook.com>
2025-09-19 22:14:55 +0100

    Add group class

---
+++ EasyManagerApp/server/EasyManagerApp.API/EasyManagerApp.API/Domain/Models/Group.cs
@@ -0,0 +1,8 @@
+﻿namespace EasyManagerApp.API.Domain.Models
+{
+    public class Group : BaseModel
+    {
+        public int Id { get; set; }
+        public string GroupName { get; set; }
+    }
+}
