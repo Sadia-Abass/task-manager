53e13733d2a7fcb5de3e432d296c8f94a7f2a697
Sadia Abass <sadia.abass@outlook.com>
2025-09-19 22:26:26 +0100

    Add generic repository interface.

---
+++ EasyManagerApp/server/EasyManagerApp.API/EasyManagerApp.API/Domain/Interfaces/IRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace EasyManagerApp.API.Domain.Interfaces
+{
+    public interface IRepository<TModel> where TModel : class
+    {
+        Task<IEnumerable<TModel>> Get();
+        Task<TModel> Get(int id);
+        Task Create(TModel model);
+        Task Update(TModel model);
+        Task Delete(int id);
+        Task CreateRange(IEnumerable<TModel> models);
+        Task UpdateRange(IEnumerable<TModel> models);
+        Task DeleteRange(IEnumerable<TModel> models);
+    }
+}
