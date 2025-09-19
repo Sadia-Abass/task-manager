0b9e1dad1dfcaa47721b4df0a54b0ca47aadfcf7
Sadia Abass <sadia.abass@outlook.com>
2025-09-19 22:09:57 +0100

    Add baseModel class to as a common class to be inherited by other classes.

---
+++ EasyManagerApp/server/EasyManagerApp.API/EasyManagerApp.API/Domain/Models/BaseModel.cs
@@ -0,0 +1,9 @@
+﻿namespace EasyManagerApp.API.Domain.Models
+{
+    public class BaseModel
+    {
+        public DateTimeKind CreatedDate { get; set; }
+        public DateTime? ModifiedDate { get; set; }
+        public bool IsDeleted { get; set; } = false;
+    }
+}
