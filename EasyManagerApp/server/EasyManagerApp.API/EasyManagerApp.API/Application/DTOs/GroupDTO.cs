58055c95be2ef1afc4b435716f108b92263a01f8
Sadia Abass <sadia.abass@outlook.com>
2025-09-20 20:16:51 +0100

    Add automapper and mapped the dto, model and view model.

--- EasyManagerApp/server/EasyManagerApp.API/EasyManagerApp.API/Application/DTOs/GroupDTO.cs
+++ EasyManagerApp/server/EasyManagerApp.API/EasyManagerApp.API/Application/DTOs/GroupDTO.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace EasyManagerApp.API.Application.DTOs
 {
-    public class Group
+    public class GroupDTO
     {
         public int Id { get; set; }
 
---
+++ EasyManagerApp/server/EasyManagerApp.API/EasyManagerApp.API/Application/Helpers/AutomapperProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using EasyManagerApp.API.Application.DTOs;
+using EasyManagerApp.API.Application.ViewModels;
+using EasyManagerApp.API.Domain.Models;
+
+namespace EasyManagerApp.API.Application.Helpers
+{
+    public class AutomapperProfile : Profile
+    {
+        public AutomapperProfile()
+        {
+            CreateMap<GroupDTO, Group>();
+            CreateMap<Group, GroupDTO>();
+            CreateMap<Group, GroupVM>();
+        }
+    }
+}
