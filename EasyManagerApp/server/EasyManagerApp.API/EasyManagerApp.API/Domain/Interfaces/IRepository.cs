69a8cdd1e92cd85650884a1aaa09a63ecdf22244
Sadia Abass <sadia.abass@outlook.com>
2025-09-20 14:29:46 +0100

    Removed Ienumerable and replace it with list.

--- EasyManagerApp/server/EasyManagerApp.API/EasyManagerApp.API/Domain/Interfaces/IRepository.cs
+++ EasyManagerApp/server/EasyManagerApp.API/EasyManagerApp.API/Domain/Interfaces/IRepository.cs
@@ -2,13 +2,13 @@
 {
     public interface IRepository<TModel> where TModel : class
     {
-        Task<IEnumerable<TModel>> Get();
+        Task<List<TModel>> Get();
         Task<TModel> Get(int id);
         void Create(TModel model);
         void Update(TModel model);
-        void Delete(int id);
-        void CreateRange(IEnumerable<TModel> models);
-        void UpdateRange(IEnumerable<TModel> models);
-        void DeleteRange(IEnumerable<TModel> models);
+        void Delete(TModel model);
+        void CreateRange(List<TModel> models);
+        void UpdateRange(List<TModel> models);
+        void DeleteRange(List<TModel> models);
     }
 }
