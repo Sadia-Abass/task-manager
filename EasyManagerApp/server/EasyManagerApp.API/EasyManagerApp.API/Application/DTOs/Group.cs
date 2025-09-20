c6b8b13bf7c18b16c9f7fb30ee12fa955fea7aba
Sadia Abass <sadia.abass@outlook.com>
2025-09-20 20:06:55 +0100

    Add group dto class.

---
+++ EasyManagerApp/server/EasyManagerApp.API/EasyManagerApp.API/Application/DTOs/Group.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EasyManagerApp.API.Application.DTOs
+{
+    public class Group
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Group Name")]
+        [Required(ErrorMessage = "{0} is required." )]
+        [StringLength(128, ErrorMessage = "{0} cannot be more than {1} characters.")]
+        public string GroupName { get; set; } = string.Empty;
+
+        [Display(Name = "Created Date")]
+        [Column(TypeName = "datetime")]
+        public DateTime CreatedDate { get; set; }
+    }
+}
