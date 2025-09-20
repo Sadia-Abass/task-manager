47546d690a34d83e11189a618799cf722f90422a
Sadia Abass <sadia.abass@outlook.com>
2025-09-20 19:58:35 +0100

    Add group view model class.

---
+++ EasyManagerApp/server/EasyManagerApp.API/EasyManagerApp.API/Application/ViewModels/GroupVM.cs
@@ -0,0 +1,9 @@
+﻿namespace EasyManagerApp.API.Application.ViewModels
+{
+    public class GroupVM
+    {
+        public int Id { get; set; }
+        public string GroupName { get; set; } = string.Empty;
+        public DateTime CreatedDate { get; set; }
+    }
+}
